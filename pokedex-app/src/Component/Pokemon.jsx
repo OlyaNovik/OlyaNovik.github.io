3539c0912835c88cca03a61cdddf9b2f7579a044
OlyaNovik <olyaanovikk@gmail.com>
2024-01-16 20:15:06 +0200

    Finish  main task

--- pokedex-app/src/Component/Pokemon.jsx
+++ pokedex-app/src/Component/Pokemon.jsx
@@ -1,13 +1,39 @@
 import s from "./PokemonStyle.module.scss"
+import React, { useState, useEffect } from 'react';
 
 const PokemonPage = (props)=>{
-    console.log(props.data);
+    const [infoPakemon, setInfoPakemon] = useState(null);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(props.data.url);
+          const data = await response.json();
+          setInfoPakemon(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+  
+      fetchData();
+    }, [props.data.url]); 
+  
+    if (!infoPakemon) {
+      return <div>Loading...</div>;
+    }
+    // console.log(infoPakemon);
     return (
-        <div className={s.container}>
+        
+        <div onClick={props.onClick} className={s.container} id={infoPakemon.id}>
             <div className={s.img_container}>
-                <img src="" alt="" />
+                <img src={infoPakemon.sprites.front_default} alt="" />
             </div>
-          <h2>{props.data.name}</h2>
+          <p className={s.Name}>{props.data.name}</p>
+          <div className={s.types}>
+          {infoPakemon.types.map((types, index) => (
+        <button key={index+2332} className={s.btn_types}>{types.type.name}</button>
+        ))}
+          </div>
         </div>
     )
 }
